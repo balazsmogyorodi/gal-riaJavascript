5d2a5e2664f6bb75f805085472890a6e02439bdb
Mogyoródi Balázs (SZF_2022_2024) <mogyorodi.balazs@diak.szamalk-szalezi.hu>
2023-03-16 15:25:23 +0100

    majdnem

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-window.addEventListener("load", init);
+window.addEventListener("load", init); 
+let hanyadikKep = 0;
+const kepek = [
+    { kep: "kepek/_DSC7025.jpg" },
+    { kep: "kepek/_DSC7371_1.jpg" },
+    { kep: "kepek/_DSC7444.jpg" }
+]
 function init() {
     tartalom = document.querySelectorAll("article");
-    const kepek = [
-        { kep: "kepek/_DSC7025.jpg" },
-        { kep: "kepek/_DSC7371_1.jpg" },
-        { kep: "kepek/_DSC7444.jpg" }
-    ]
-    articleResz(tartalom, kepek);
+    
+    articleResz(tartalom);
  
-    kepKattintas(kepek);
+    kepKattintas();
 
 }
 
-function articleResz(tartalom, kepek) {
+function articleResz(tartalom) {
     let kiiras = "";
     for (let index = 0; index < kepek.length; index++) {
         kiiras += "<div class='kisKepek'>"
@@ -24,31 +26,28 @@ function articleResz(tartalom, kepek) {
     tartalom[0].innerHTML += kiiras;
 }
 
-function kepKattintas(kepek) {
-    let hanyadikKep = 0;
+function kepKattintas() {
+   
     galleria = document.getElementsByClassName("nagykep");
     let kiiras = "";
     kiiras = `<img src="${kepek[hanyadikKep].kep}" alt='kép' class='foKep'>`;
     galleria[0].innerHTML += kiiras;
     kattintasBal = document.getElementById("bal");
     kattintasJobb = document.getElementById("jobb");
-        kattintasJobbra(hanyadikKep, kepek);
+    kattintasJobb.addEventListener("click",kattintasJobbra)
         kattintasBalra(hanyadikKep, kepek);
        
-    
-   
-
 
 }
 
-function kattintasJobbra(hanyadikKep, kepek) {
-
-
+function kattintasJobbra() {
     hanyadikKep += 1;
-    if (hanyadikKep > kepek.length) {
+    if (hanyadikKep > kepek.length-1) {
         hanyadikKep = 0;
     }
-
+    galleria = document.getElementsByClassName("nagykep");
+    kiiras = `<img src="${kepek[hanyadikKep].kep}" alt='kép' class='foKep'>`;
+    galleria[0].innerHTML = kiiras;
 
 }
 
